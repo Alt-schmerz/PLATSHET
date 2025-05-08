968c7fecfac599bd528edc484bd5c40b42de0c4d
Altschmerz <ajdbimmuyag2020@gmail.com>
2025-05-08 14:10:42 +0800

    delete

--- TerraVive/app/src/main/java/com/example/terravive/ApprovedActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.terravive
-
-data class ApprovedActivity(
-    val date: String = "",
-    val location: String = "",
-    val name: String = "",
-    val time: String = ""
-)
--- TerraVive/app/src/main/java/com/example/terravive/NotificationItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.terravive
-
-data class NotificationItem(
-    var username: String = "",
-    var message: String = "",
-    var profileImageResource: Int = 0,
-    var contentImageResource: Int = 0,
-    var category: String = ""
-)
